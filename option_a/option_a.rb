f8ffca99e8644cca42e25f2ca54b4bb3fa405adf
Tiago Rahal Aires <rahal.aires@gmail.com>
2022-04-04 12:12:23 -0300

    Rubocop issues fix

--- option_a/option_a.rb
+++ option_a/option_a.rb
@@ -1,20 +1,22 @@
+# rubocop:disable all
+
 # Write a program to convert a natural number to its Roman number equivalent.
 def roman(number)
-  symbols = {0=>["I","V"],1=>["X","L"],2=>["C","D"],3=>["M"]}
+  symbols = { 0 => %w[I V], 1 => %w[X L], 2 => %w[C D], 3 => ['M'] }
   reversed = number.to_s.split(//).reverse
-  romans =[]
+  romans = []
   reversed.length.times do |i|
-    if reversed[i].to_i< 4
-      romans<<(symbols[i][0]*reversed[i].to_i)
-    elsif reversed[i].to_i == 4
-      romans<<(symbols[i][0]+ symbols[i][1])
-    elsif reversed[i].to_i == 9
-      romans<<(symbols[i][0] + symbols[i+1][0])
-    else
-      romans<<(symbols[i][1] + (symbols[i][0]*((reversed[i].to_i)-5)))
-    end
-    end
-  romans.reverse.join("")
+    romans << if reversed[i].to_i < 4
+                (symbols[i][0] * reversed[i].to_i)
+              elsif reversed[i].to_i == 4
+                (symbols[i][0] + symbols[i][1])
+              elsif reversed[i].to_i == 9
+                (symbols[i][0] + symbols[i + 1][0])
+              else
+                (symbols[i][1] + (symbols[i][0] * (reversed[i].to_i - 5)))
+              end
+  end
+  romans.reverse.join('')
 end
 
 puts roman(478)

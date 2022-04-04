e16d53560e76c404ee5e5b00e122b5c17006c259
Tiago Rahal Aires <rahal.aires@gmail.com>
2022-04-04 16:53:56 -0300

    Rubocop issues fix

--- option_b/option_b.rb
+++ option_b/option_b.rb
@@ -1,70 +1,65 @@
-def number_to_words(num=nil)
-  
+# rubocop:disable all
+
+def number_to_words(num = nil)
   len = num
 
-  if len.to_s.size === 0 
-    puts "Invalid."
-  end
+  puts 'Invalid.' if len.to_s.size === 0
 
-  if len.to_s.size > 4
-    puts "Max of 9999 please."
-  end
+  puts 'Max of 9999 please.' if len.to_s.size > 4
 
-  single_digit = [
-    "zero",
-    "one",
-    "two",
-    "three",
-    "four",
-    "five",
-    "six",
-    "seven",
-    "eight",
-    "nine",
+  single_digit = %w[
+    zero
+    one
+    two
+    three
+    four
+    five
+    six
+    seven
+    eight
+    nine
   ]
 
   double_digit = [
-    "",
-    "ten",
-    "eleven",
-    "twelve",
-    "thirteen",
-    "fourteen",
-    "fifteen",
-    "sixteen",
-    "seventeen",
-    "eighteen",
-    "nineteen",
+    '',
+    'ten',
+    'eleven',
+    'twelve',
+    'thirteen',
+    'fourteen',
+    'fifteen',
+    'sixteen',
+    'seventeen',
+    'eighteen',
+    'nineteen'
   ]
 
   tens_multiple = [
-    "",
-    "",
-    "twenty",
-    "thirty",
-    "forty",
-    "fifty",
-    "sixty",
-    "seventy",
-    "eighty",
-    "ninety",
+    '',
+    '',
+    'twenty',
+    'thirty',
+    'forty',
+    'fifty',
+    'sixty',
+    'seventy',
+    'eighty',
+    'ninety'
   ]
 
-  tens_power = ["hundred", "thousand"]
+  tens_power = %w[hundred thousand]
 
-  if len.to_s.size === 1    
-    puts single_digit[num]
-  end
+  puts single_digit[num] if len.to_s.size === 1
 
   if len.to_s.size === 2
     n1 = len.to_s[0]
     n2 = len.to_s[1]
     if n1.to_i === 1
       puts double_digit[n2.to_i + 1]
-    elsif n1.to_i > 1 and n2.to_i > 0
-      double_digit_number = tens_multiple[n1.to_i] + " " + single_digit[n2.to_i]
+    elsif (n1.to_i > 1) && n2.to_i.positive?
+      double_digit_number = "#{tens_multiple[n1.to_i]} #{single_digit[n2.to_i]}"
       puts double_digit_number
-    elsif n1.to_i > 1 and n2.to_i === 0
+    elsif (n1.to_i > 1) && (n2.to_i === 0)
       double_digit_number = tens_multiple[n1.to_i]
       puts double_digit_number
     end
@@ -75,14 +70,14 @@ def number_to_words(num=nil)
     n2 = len.to_s[1]
     n3 = len.to_s[2]
     if n2.to_i === 1
-      three_digits = single_digit[n1.to_i] + " " + tens_power[0] + " and " + double_digit[n2.to_i + 2]  
+      three_digits = "#{single_digit[n1.to_i]} #{tens_power[0]} and #{double_digit[n2.to_i + 2]}"
       puts three_digits
-    elsif n2.to_i > 1 and n3.to_i > 0
-      three_digit_number = single_digit[n1.to_i] + " " + tens_power[0] + " and " + tens_multiple[n2.to_i] + " " + single_digit[n3.to_i]
+    elsif (n2.to_i > 1) && n3.to_i.positive?
+      three_digit_number = "#{single_digit[n1.to_i]} #{tens_power[0]} and #{tens_multiple[n2.to_i]} #{single_digit[n3.to_i]}"
       puts three_digit_number
-    elsif n2.to_i > 1 and n3.to_i === 0
-      n3 = " " 
-      three_digit_number = single_digit[n1.to_i] + " " + tens_power[0] + " and " + tens_multiple[n2.to_i]
+    elsif (n2.to_i > 1) && (n3.to_i === 0)
+      n3 = ' '
+      three_digit_number = "#{single_digit[n1.to_i]} #{tens_power[0]} and #{tens_multiple[n2.to_i]}"
       puts three_digit_number
     end
   end
@@ -93,24 +88,23 @@ def number_to_words(num=nil)
     n3 = len.to_s[2]
     n4 = len.to_s[3]
     # the last two digits: between 10 and 19
-    if n3.to_i === 1 and n4.to_i > 0
-      four_digits = single_digit[n1.to_i] + " " + tens_power[1] + " " + single_digit[n2.to_i] + " " + tens_power[0] + " and " + double_digit[n4.to_i + 1]
+    if (n3.to_i === 1) && n4.to_i.positive?
+      four_digits = "#{single_digit[n1.to_i]} #{tens_power[1]} #{single_digit[n2.to_i]} #{tens_power[0]} and #{double_digit[n4.to_i + 1]}"
       puts four_digits
-    elsif n3.to_i > 1 and n4.to_i > 0
-      four_digit_number = single_digit[n1.to_i] + " " + tens_power[1] + " " + single_digit[n2.to_i] + " " + tens_power[0] + " and " + tens_multiple[n3.to_i] + " " + single_digit[n4.to_i]
+    elsif (n3.to_i > 1) && n4.to_i.positive?
+      four_digit_number = "#{single_digit[n1.to_i]} #{tens_power[1]} #{single_digit[n2.to_i]} #{tens_power[0]} and #{tens_multiple[n3.to_i]} #{single_digit[n4.to_i]}"
       puts four_digit_number
-    elsif n3.to_i > 1 and n4.to_i === 0
-      four_digit_number = single_digit[n1.to_i] + " " + tens_power[1] + " " + single_digit[n2.to_i] + " " + tens_power[0] + " and " + tens_multiple[n3.to_i]
+    elsif (n3.to_i > 1) && (n4.to_i === 0)
+      four_digit_number = "#{single_digit[n1.to_i]} #{tens_power[1]} #{single_digit[n2.to_i]} #{tens_power[0]} and #{tens_multiple[n3.to_i]}"
       puts four_digit_number
-    elsif n3.to_i === 0 and n4.to_i === 0
-      four_digit_number = single_digit[n1.to_i] + " " + tens_power[1] + " and " + single_digit[n2.to_i] + " " + tens_power[0]
+    elsif (n3.to_i === 0) && (n4.to_i === 0)
+      four_digit_number = "#{single_digit[n1.to_i]} #{tens_power[1]} and #{single_digit[n2.to_i]} #{tens_power[0]}"
       puts four_digit_number
-    elsif n3.to_i === 1 and n4.to_i === 0
-      four_digit_number = single_digit[n1.to_i] + " " + tens_power[1] + " " + single_digit[n2.to_i] + " " + tens_power[0] + " and " + double_digit[n4.to_i + 1]
+    elsif (n3.to_i === 1) && (n4.to_i === 0)
+      four_digit_number = "#{single_digit[n1.to_i]} #{tens_power[1]} #{single_digit[n2.to_i]} #{tens_power[0]} and #{double_digit[n4.to_i + 1]}"
       puts four_digit_number
     end
   end
-    
 end
 
 number_to_words(232)

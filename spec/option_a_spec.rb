e97925138ff1a7d27840910cf4ef3c5f64c1b77f
Tiago Rahal Aires <rahal.aires@gmail.com>
2022-04-04 17:09:06 -0300

    Add mock template to specs

---
+++ spec/option_a_spec.rb
@@ -0,0 +1,29 @@
+require_relative '../option_a/option_a.rb'
+
+describe Option_A do
+
+  option_a = Option_A.new
+
+  context 'Test factorial method' do
+    it 'Returns 24 if the argument is 4' do
+      expect(solver.factorial(4)).to eq 24
+    end
+
+    it 'Return 720 if the argument is 6' do
+      expect(solver.factorial(6)).to eq 720
+    end
+
+  end
+
+  context 'Test FizzBuzz method' do
+    it 'Returns fizz if the number passed is divisible by three' do
+      expect(solver.fizzbuzz(3)).to eq 'fizz'
+    end
+
+    it 'Returns buzz if the number passed is divisible by five' do
+      expect(solver.fizzbuzz(5)).to eq 'buzz'
+    end
+
+    end
+  end
+end

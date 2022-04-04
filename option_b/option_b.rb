3b8ff1f0c406a21b97372276c1617d6e3d8a2c34
Tiago Rahal Aires <rahal.aires@gmail.com>
2022-04-04 16:22:46 -0300

    Finish numbers until 999

--- option_b/option_b.rb
+++ option_b/option_b.rb
@@ -69,6 +69,23 @@ def number_to_words(num)
       puts double_digit_number
     end
   end
+
+  if len.to_s.size === 3
+    n1 = len.to_s[0]
+    n2 = len.to_s[1]
+    n3 = len.to_s[2]
+    if n2.to_i === 1
+      three_digits = single_digit[n1.to_i] + " " + tens_power[0] + " and " + double_digit[n2.to_i + 2]  
+      puts three_digits
+    elsif n2.to_i > 1 and n3.to_i > 0
+      three_digit_number = single_digit[n1.to_i] + " " + tens_power[0] + " and " + tens_multiple[n2.to_i] + " " + single_digit[n3.to_i]
+      puts three_digit_number
+    elsif n2.to_i > 1 and n3.to_i === 0
+      n3 = " " 
+      three_digit_number = single_digit[n1.to_i] + " " + tens_power[0] + " and " + tens_multiple[n2.to_i]
+      puts three_digit_number
+    end
+  end
     
 end
 

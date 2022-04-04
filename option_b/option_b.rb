b6572d761ca9ad3b0d85fab954e0865186ca9f97
Tiago Rahal Aires <rahal.aires@gmail.com>
2022-04-04 15:09:02 -0300

    One digit number finish

--- option_b/option_b.rb
+++ option_b/option_b.rb
@@ -1,1 +1,61 @@
-# Option B
+def number_to_words(num=0)
+  
+  len = num
+
+  if len.to_s.size === 0 
+    puts "Invalid."
+  end
+
+  if len.to_s.size > 4
+    puts "Max of 9999 please."
+  end
+
+  single_digit = [
+    "zero",
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+  ]
+
+  double_digit = [
+    "",
+    "ten",
+    "eleven",
+    "twelve",
+    "thirteen",
+    "fourteen",
+    "fifteen",
+    "sixteen",
+    "seventeen",
+    "eighteen",
+    "nineteen",
+  ]
+
+  tens_multiple = [
+    "",
+    "",
+    "twenty",
+    "thirty",
+    "forty",
+    "fifty",
+    "sixty",
+    "seventy",
+    "eighty",
+    "ninety",
+  ]
+
+  tens_power = ["hundred", "thousand"]
+
+  if len.to_s.size === 1    
+    puts single_digit[num]
+  end
+    
+end
+
+number_to_words(5)
